4cd664ab370c93100f2d05b0a80b640629fbfbe2
wintoo <wintoo143@gmail.com>
2017-12-10 23:24:04 -0600

    This commit

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(new App().greet("world"));
-    }
-
-    public String greet(String name) {
-        return "Hello " + name;
+        System.out.println("Hello World!");
     }
 }
--- src/test/java/com/github/jitpack/AppTest.java
+++ src/test/java/com/github/jitpack/AppTest.java
@@ -1,22 +1,37 @@
 package com.github.jitpack;
 
-import org.junit.Test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+/**
+ * Unit test for simple App.
+ */
 public class AppTest 
+    extends TestCase
 {
-    App app = new App();
-
-    @Test
-    public void testApp()
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
     {
-        assertEquals("Hello world", app.greet("world"));
+        super( testName );
     }
 
-    @Test
-    public void testTrue()
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
     {
         assertTrue( true );
     }
